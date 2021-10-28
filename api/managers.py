95e59f281daa78260e7feba73b861553e2bbd8bf
S0ltero <AlexsSobko61@gmail.com>
2021-10-28 18:54:46 +0300

    Now custom managers inherited from `BaseUserManager`

--- api/managers.py
+++ api/managers.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager
 
 
-class StudentManager(models.Manager):
+class StudentManager(BaseUserManager):
     def get_queryset(self):
         return super().get_queryset().filter(is_trainer=False)
 
 
-class TrainerManager(models.Manager):
+class TrainerManager(BaseUserManager):
     def get_queryset(self):
         return super().get_queryset().filter(is_trainer=True)
