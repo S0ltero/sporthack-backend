dd656d9851271566ec0c0cf4f552c9f4971718bd
S0ltero <AlexsSobko61@gmail.com>
2021-10-28 18:47:42 +0300

    Add imports to `views.py`

--- api/views.py
+++ api/views.py
@@ -1,1 +1,23 @@
-from django.shortcuts import render
+from rest_framework.generics import (
+    CreateAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+    RetrieveUpdateDestroyAPIView,
+    ListAPIView,
+)
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+
+from .models import (
+    User, 
+    Student, Trainer, 
+    Section, SectionMember,
+    SectionEvent
+)
+from .serializers import (
+    UserSerializer,
+    StudentSerializer, TrainerSerializer,
+    SectionSerializer, SectionMemberSerializer,
+    SectionEventSerializer
+)
