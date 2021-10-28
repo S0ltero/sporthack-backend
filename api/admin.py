1dca466657d1fd3e7db401e24f33de5333fa7152
S0ltero <AlexsSobko61@gmail.com>
2021-10-28 18:56:48 +0300

    Add `SectionMemberList`

--- api/admin.py
+++ api/admin.py
@@ -1,1 +1,11 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .models import *
+
+
+class SectionMemberList(admin.StackedInline):
+    model = SectionMember
+    extra = 0
+    classes = ["collapse"]
