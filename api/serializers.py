c7ae7df6c523f44082d790ae2fb3779455844434
S0ltero <AlexsSobko61@gmail.com>
2021-10-29 17:34:58 +0300

    Set `username` as non-required field Set list of fields to return

--- api/serializers.py
+++ api/serializers.py
@@ -15,7 +15,12 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = "__all__"
+        fields = (
+            "id", "username", "email",
+            "phone", "institution", "group",
+            "image", "rank"
+        )
+        extra_kwargs = {"username": {"required": False}}
 
 
 class StudentSerializer(serializers.ModelSerializer):
