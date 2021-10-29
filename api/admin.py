e0427d648ba1b3de3c7282983adb8050af9a68ba
S0ltero <AlexsSobko61@gmail.com>
2021-10-29 15:52:25 +0300

    Add admin model for `Student` model

--- api/admin.py
+++ api/admin.py
@@ -22,6 +22,28 @@ class EventMemberList(admin.StackedInline):
     extra = 0
     classes = ["collapse"]
 
+
+@admin.register(Student)
+class AdminStudent(UserAdmin):
+    actions = None
+    list_display = ("id", "username", "institution", "group", "is_active")
+    list_filter = ("is_active",)
+    list_display_links = ("username",)
+    readonly_fields = ("sex", "password", "last_login", "date_joined")
+    fieldsets = (
+        ("Основная информация", {"fields": (("username", "email", "sex", "password"))}),
+        ("Учебное заведение", {"fields": ("group", "institution")}),
+        ("Дополнительная информация", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "fields": ("username", "email", "password1", "password2", "sex")}
+         ),
+    )
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        return queryset.filter(is_trainer=False)
 @admin.register(SectionEvent)
 class AdminSectionEvent(admin.ModelAdmin):
     inlines = [EventMemberList]
