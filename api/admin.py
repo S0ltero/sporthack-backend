c2f526e73eb5a02da350614720f0c8306b18cafa
S0ltero <AlexsSobko61@gmail.com>
2021-10-30 15:15:04 +0300

    Update admin model of `Section` - Add `list_display` - Add `list_display_links`

--- api/admin.py
+++ api/admin.py
@@ -73,6 +73,8 @@ class AdminTrainer(UserAdmin):
 @admin.register(Section)
 class AdminSection(admin.ModelAdmin):
     inlines = [SectionMemberList]
+    list_display = ("id", "title")
+    list_display_links = ("id", "title")
     actions = None
 
 
