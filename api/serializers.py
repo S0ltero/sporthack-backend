6c669e60d6d88a506579da0231c104b02eec4d4a
S0ltero <AlexsSobko61@gmail.com>
2021-12-09 13:18:23 +0300

    Add `LoginSerializer`

--- api/serializers.py
+++ api/serializers.py
@@ -1,5 +1,6 @@
 import base64
 
+from django.contrib.auth import authenticate
 from django.utils import timezone
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
@@ -240,3 +241,16 @@ class TokenSerializer(serializers.ModelSerializer):
     class Meta:
         model = Token
         fields = ("auth_token", "id", "is_trainer")
+
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.CharField()
+    password = serializers.CharField(
+        write_only=True, required=True, style={"input_type": "password"}
+    )
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError('Incorrect Credentials Passed.')
