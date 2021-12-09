c9a475e5a21322b33323aff27e82bb66dda26a4a
S0ltero <AlexsSobko61@gmail.com>
2021-12-09 16:17:20 +0300

    Remove unused import

--- api/models.py
+++ api/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core import validators
-from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from phonenumber_field.modelfields import PhoneNumberField
 
