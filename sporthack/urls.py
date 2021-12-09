fd2314c5f61dc1cf16a1552c95dc9f84c571f99a
S0ltero <AlexsSobko61@gmail.com>
2021-12-09 13:20:05 +0300

    Update `urls.py` - Change `djoser.urls` for login to `knox.urls` - Now use `re_path`

--- sporthack/urls.py
+++ sporthack/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
+
+from api.views import LoginAPI
+from knox import views as knox_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('djoser.urls')),
-    path('auth/', include('djoser.urls.authtoken')),
-    path('auth/', include('djoser.urls.jwt')),
-    path("api/", include("api.urls"))
+    re_path(r'auth/token/login/', LoginAPI.as_view(), name="login"),
+    re_path(r'auth/token/logout', knox_views.LogoutView.as_view(), name='logout'),
+    re_path(r'auth/toke/logoutall', knox_views.LogoutAllView.as_view(), name='logoutall'),
+    path("api/", include("api.urls")),
 ]
 
 handler404 = "api.views.handler404"
