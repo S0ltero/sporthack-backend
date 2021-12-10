efe45ccec1c111d27ac06fda17acb8ddd0d75f95
S0ltero <AlexsSobko61@gmail.com>
2021-12-10 15:58:50 +0300

    Add signals for sending email on verified awards

---
+++ api/signals.py
@@ -0,0 +1,26 @@
+from django.db.models.signals import pre_save, post_delete
+from django.dispatch import receiver
+from django.conf import settings
+
+from .models import StudentAward
+from .email import AwardSuccessVerified, AwardNoVerified
+
+
+@receiver(pre_save, sender=StudentAward)
+def on_award_update(sender, instance, **kwargs):
+    if instance.id is None:
+        return
+
+    current = instance
+    previous = StudentAward.objects.get(pk=instance.pk)
+    if previous.verified != current.verified and current.verified == True:
+        context = {"award_title": instance.title, "domain": settings.SITE_DOMAIN}
+        to = [instance.user.email]
+        AwardSuccessVerified(context=context).send(to)
+
+
+@receiver(post_delete, sender=StudentAward)
+def on_award_delete(sender, instance, **kwargs):
+    context = {"award_title": instance.title, "domain": settings.SITE_DOMAIN}
+    to = [instance.user.email]
+    AwardNoVerified(context=context).send(to)
