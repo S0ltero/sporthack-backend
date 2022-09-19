a8395bc49453038ddc1c4300a207f64a386d0754
S0ltero <AlexsSobko61@gmail.com>
2022-09-19 11:30:27 +0300

    Remove `sections` field from `TrainerSerializer`

--- api/serializers.py
+++ api/serializers.py
@@ -91,14 +91,12 @@ class SectionSerializer(serializers.ModelSerializer):
 
 class TrainerSerializer(serializers.ModelSerializer):
     photo = CustomBase64ImageField(represent_in_base64=True, required=False)
-    sections = SectionSerializer(read_only=True, many=True)
 
     class Meta:
         model = Trainer
         fields = (
             "id", "last_name", "first_name", "middle_name",
             "email", "phone", "rank", "photo", "is_trainer",
-            "sections"
         )
 
 
